b623c75275a858f25143d4d2bd87a14ca371df54
Thomas Merkel <tm@core.io>
2014-10-19 16:38:57 +0200

    Create delegate dataset for configuration file

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/${UUID}/data
+
+if zfs list ${DDS} 1>/dev/null 2>&1; then
+	zfs create ${DDS}/etc_ircd
+	zfs set mountpoint=/opt/local/etc/ircd ${DDS}/etc_ircd
+	chown ircd:ircd /opt/local/etc/ircd || true
+fi
