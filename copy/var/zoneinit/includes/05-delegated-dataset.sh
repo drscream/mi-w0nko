f5e5075ad91f6f72909a5cafa915c415c94d3e02
Thomas Merkel <tm@core.io>
2021-05-28 11:26:57 +0200

    Fix problem with delegate dataset exists

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -3,7 +3,7 @@ UUID=$(mdata-get sdc:uuid)
 DDS=zones/${UUID}/data
 
 if zfs list ${DDS} 1>/dev/null 2>&1; then
-	zfs create ${DDS}/etc_ircd
+	zfs create ${DDS}/etc_ircd || true
 	zfs set mountpoint=/opt/local/etc/ircd ${DDS}/etc_ircd
 	chown ircd:ircd /opt/local/etc/ircd || true
 fi
